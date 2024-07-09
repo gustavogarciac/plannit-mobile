ff3e203cd070a1deae3e6bb3a0be4e281634dc9e
gustavo <gustavogarciaci1@gmail.com>
2024-07-09 19:57:55 -0300

    chore: initial setup

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+export const Index = (props: Props) => {
+  return (
+    <View>
+      <Text>Hello, Plannit!</Text>
+    </View>
+  )
+}
+
+export default Index
